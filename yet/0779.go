7b50a6ba7941440820e38de9153830f43f8c2cf0
遠山篤史 <atsushi.tohyama.160.333@gmail.com>
2019-06-10 11:04:33 +0900

    Add 0779.go

--- yet/0779.go
+++ yet/0779.go
@@ -1,5 +1,33 @@
+// Explanation:
+// row 1: 0
+// row 2: 01
+// row 3: 0110
+// row 4: 01101001
+// row 5: 0110100110010110
+
+// i >= 2のとき
+// row i  : XY
+// row i+1: XYYX
+
+func rec(now, row int, s string) string {
+	if now == row {
+		return s
+	}
+	mid := len(s) / 2
+	head := s[:mid]
+	tail := s[mid:]
+	s = rec(now+1, row, head+tail+tail+head)
+	return s
+}
+
+// N = 30
+// K = 434991989
+// のときにruntime error
+
 func kthGrammar(N int, K int) int {
-	if n == 1 {
+	if N == 1 {
 		return 0
 	}
+	res := rec(2, N, "01")
+    return int(res[K-1] - '0')
 }
