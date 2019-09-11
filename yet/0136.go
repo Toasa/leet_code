d499c5dd6d9ab8e16b6716d7ad5e1200b7ae21af
遠山篤史 <atsushi.tohyama.160.333@gmail.com>
2019-09-11 09:54:13 +0900

    Update 0136

--- yet/0136.go
+++ yet/0136.go
@@ -1,14 +1,7 @@
 func singleNumber(nums []int) int {
-	sort.Ints(nums)
-
-	for i := 0; i < len(nums) - 2; {
-		if nums[i] != nums[i + 1] {
-			return nums[i]
-		}
-		old := nums[i]
-		for nums[i] == old {
-			i++
-		}
+	num := nums[0]
+	for i := 0; i < len(nums); i++ {
+		num ^= nums[i]
 	}
-	return nums[len(nums) - 1]
+	return num\
 }
